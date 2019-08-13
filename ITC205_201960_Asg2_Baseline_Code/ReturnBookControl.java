c6b824cc8591b10b70decf384eacd86ef9bb0e52
WIMALARATNE Dilkushi Nisansala <11651108@students.local>
2019-08-13 14:31:45 +1000

    Changed ReturnBookControl.java to manage coding standards

--- ITC205_201960_Asg2_Baseline_Code/ReturnBookControl.java
+++ ITC205_201960_Asg2_Baseline_Code/ReturnBookControl.java
@@ -2,33 +2,33 @@ public class ReturnBookControl {
 
 	private ReturnBookUI Ui;
 	private enum CONTROL_STATE { INITIALISED, READY, INSPECTING };
-	private CONTROL_STATE sTaTe;
+	private CONTROL_STATE state;//changed sTaTe to state-Dilkushi
 	
-	private library lIbRaRy;
-	private loan CurrENT_loan;
+	private library library_;//changed lIbRaRy to library_--Dilkushi
+	private loan current_loan;//changed CurrENT_loan to current_loan--Dilkushi
 	
 
 	public ReturnBookControl() {
-		this.lIbRaRy = lIbRaRy.INSTANCE();
-		sTaTe = CONTROL_STATE.INITIALISED;
+		this.library_ = library_.INSTANCE();//changed lIbRaRy to library_--Dilkushi
+		state = CONTROL_STATE.INITIALISED;//changed sTaTe to state--Dilkushi
 	}
 	
 	
 	public void Set_UI(ReturnBookUI ui) {
-		if (!sTaTe.equals(CONTROL_STATE.INITIALISED)) {
+		if (!state.equals(CONTROL_STATE.INITIALISED)) {//changed sTaTe to state--Dilkushi
 			throw new RuntimeException("ReturnBookControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.Ui = ui;
 		ui.Set_State(ReturnBookUI.UI_STATE.READY);
-		sTaTe = CONTROL_STATE.READY;		
+		state = CONTROL_STATE.READY;//changed sTaTe to state--Dilkushi		
 	}
 
 
 	public void Book_scanned(int Book_ID) {
-		if (!sTaTe.equals(CONTROL_STATE.READY)) {
+		if (!state.equals(CONTROL_STATE.READY)) {//changed sTaTe to state--Dilkushi
 			throw new RuntimeException("ReturnBookControl: cannot call bookScanned except in READY state");
 		}	
-		book CUR_book = lIbRaRy.Book(Book_ID);
+		book CUR_book = library_.Book(Book_ID);//changed lIbRaRy to library_--Dilkushi
 		
 		if (CUR_book == null) {
 			Ui.display("Invalid Book Id");
@@ -38,25 +38,25 @@ public class ReturnBookControl {
 			Ui.display("Book has not been borrowed");
 			return;
 		}		
-		CurrENT_loan = lIbRaRy.LOAN_BY_BOOK_ID(Book_ID);	
+		current_loan = library_.LOAN_BY_BOOK_ID(Book_ID);//changed CurrENT_loan to current_loan--Dilkushi //changed lIbRaRy to library_--Dilkushi	
 		double Over_Due_Fine = 0.0;
-		if (CurrENT_loan.OVer_Due()) {
-			Over_Due_Fine = lIbRaRy.CalculateOverDueFine(CurrENT_loan);
+		if (current_loan.OVer_Due()) {//changed CurrENT_loan to current_loan--Dilkushi	
+			Over_Due_Fine = library_.CalculateOverDueFine(current_loan);//changed CurrENT_loan to current_loan--Dilkushi //changed lIbRaRy to library_--Dilkushi	
 		}
 		Ui.display("Inspecting");
 		Ui.display(CUR_book.toString());
-		Ui.display(CurrENT_loan.toString());
+		Ui.display(current_loan.toString());//changed CurrENT_loan to current_loan--Dilkushi	
 		
-		if (CurrENT_loan.OVer_Due()) {
+		if (current_loan.OVer_Due()) {//changed CurrENT_loan to current_loan--Dilkushi	
 			Ui.display(String.format("\nOverdue fine : $%.2f", Over_Due_Fine));
 		}
 		Ui.Set_State(ReturnBookUI.UI_STATE.INSPECTING);
-		sTaTe = CONTROL_STATE.INSPECTING;		
+		state = CONTROL_STATE.INSPECTING;//changed sTaTe to state--Dilkushi		
 	}
 
 
 	public void Scanning_Complete() {
-		if (!sTaTe.equals(CONTROL_STATE.READY)) {
+		if (!state.equals(CONTROL_STATE.READY)) {//changed sTaTe to state--Dilkushi
 			throw new RuntimeException("ReturnBookControl: cannot call scanningComplete except in READY state");
 		}	
 		Ui.Set_State(ReturnBookUI.UI_STATE.COMPLETED);		
@@ -64,13 +64,13 @@ public class ReturnBookControl {
 
 
 	public void Discharge_loan(boolean isDamaged) {
-		if (!sTaTe.equals(CONTROL_STATE.INSPECTING)) {
+		if (!state.equals(CONTROL_STATE.INSPECTING)) {//changed sTaTe to state--Dilkushi
 			throw new RuntimeException("ReturnBookControl: cannot call dischargeLoan except in INSPECTING state");
 		}	
-		lIbRaRy.Discharge_loan(CurrENT_loan, isDamaged);
-		CurrENT_loan = null;
+		lIbRaRy.Discharge_loan(current_loan, isDamaged);
+		current_loan = null;//changed CurrENT_loan to current_loan--Dilkushi	
 		Ui.Set_State(ReturnBookUI.UI_STATE.READY);
-		sTaTe = CONTROL_STATE.READY;				
+		state = CONTROL_STATE.READY;	//changed sTaTe to state--Dilkushi			
 	}
 
 
