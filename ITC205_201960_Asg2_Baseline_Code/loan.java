a27194b6417f4a5b9e301122a875343298c6d76f
darshan-thanuj <52956188+darshan-thanuj@users.noreply.github.com>
2019-08-20 22:51:47 +1000

    fixed errors in code
    fixed errors in the code.

--- ITC205_201960_Asg2_Baseline_Code/loan.java
+++ ITC205_201960_Asg2_Baseline_Code/loan.java
@@ -5,44 +5,44 @@ import java.util.Date;
 @SuppressWarnings("serial")
 public class loan implements Serializable {
 	
-	public static enum LOAN_STATE { CURRENT, OVER_DUE, DISCHARGED };
+	public static enum LoanState { CURRENT, OVER_DUE, DISCHARGED }; // Changed LoanState to LoanState , Darshan - 08/20/2019 7:17 pm
 	
-	private int ID;
-	private book B;
-	private member M;
-	private Date D;
-	private LOAN_STATE state;
+	private int id; // Changed ID to id , Darshan - 08/20/2019 7:17 pm
+	private Book b; // Changed B to b , Darshan - 08/20/2019 7:17 pm, Book to Book
+	private Member M; // Changed M to m, Member to Member , Darshan - 08/20/2019 7:17 pm
+	private Date d; // Changed d to d , Darshan - 08/20/2019 7:20 pm
+	private LoanState state; // Changed LoanState to LoanState , Darshan - 08/20/2019 7:20 pm
 
 	
-	public loan(int loanId, book book, member member, Date dueDate) {
-		this.ID = loanId;
-		this.B = book;
-		this.M = member;
-		this.D = dueDate;
-		this.state = LOAN_STATE.CURRENT;
+	public loan(int loanId, Book book, Member member, Date dueDate) {
+		this.id = loanId;
+		this.b = book;
+		this.m = member;
+		this.d = dueDate;
+		this.state = LoanState.CURRENT;
 	}
 
 	
 	public void checkOverDue() {
-		if (state == LOAN_STATE.CURRENT &&
-			Calendar.INSTANCE().Date().after(D)) {
-			this.state = LOAN_STATE.OVER_DUE;			
+		if (state == LoanState.CURRENT &&
+			Calendar.INSTANCE().Date().after(d)) {
+			this.state = LoanState.OVER_DUE;			
 		}
 	}
 
 	
-	public boolean OVer_Due() {
-		return state == LOAN_STATE.OVER_DUE;
+	public boolean overDue() { // Changed OVer_Due to overDue , Darshan - 08/20/2019 7:20 pm
+		return state == LoanState.OVER_DUE;
 	}
 
 	
-	public Integer ID() {
-		return ID;
+	public Integer id() {
+		return id;
 	}
 
 
-	public Date Get_Due_Date() {
-		return D;
+	public Date getDueDate() { // Changed Get_Due_Date to getDueDate , Darshan - 08/20/2019 7:20 pm
+		return d;
 	}
 	
 	
@@ -50,29 +50,30 @@ public class loan implements Serializable {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		StringBuilder sb = new StringBuilder();
-		sb.append("Loan:  ").append(ID).append("\n")
-		  .append("  Borrower ").append(M.GeT_ID()).append(" : ")
-		  .append(M.Get_LastName()).append(", ").append(M.Get_FirstName()).append("\n")
-		  .append("  Book ").append(B.ID()).append(" : " )
-		  .append(B.TITLE()).append("\n")
-		  .append("  DueDate: ").append(sdf.format(D)).append("\n")
+		// Changed M to m, TITLE() to title, Get_LastName() to getLastName, Darshan - 08/20/2019 7:25 pm
+		sb.append("Loan:  ").append(id).append("\n")
+		  .append("  Borrower ").append(m.getId()).append(" : ")
+		  .append(M.getLastName()).append(", ").append(m.getFirstName()).append("\n")
+		  .append("  Book ").append(b.id()).append(" : " )
+		  .append(b.title()).append("\n")
+		  .append("  DueDate: ").append(sdf.format(d)).append("\n")
 		  .append("  State: ").append(state);		
 		return sb.toString();
 	}
 
 
-	public member Member() {
-		return M;
+	public Member getMember() { // Changed Member() to getMember() , Darshan - 08/20/2019 7:25 pm
+		return m;
 	}
 
 
-	public book Book() {
-		return B;
+	public Book book() {
+		return b;
 	}
 
 
-	public void DiScHaRgE() {
-		state = LOAN_STATE.DISCHARGED;		
+	public void discharge() { // Changed DiScHaRgE() to discharge() ,Darshan - 08/20/2019 7:25 pm
+		state = LoanState.DISCHARGED;		
 	}
 
 }
