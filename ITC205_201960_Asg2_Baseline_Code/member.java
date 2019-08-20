f462e7730094775362cf3bc4e3f2b8db6074bd8d
darshan-thanuj <52956188+darshan-thanuj@users.noreply.github.com>
2019-08-20 23:56:55 +1000

    update the code
    fixed errors in the code by renaming variables accordingly.

--- ITC205_201960_Asg2_Baseline_Code/member.java
+++ ITC205_201960_Asg2_Baseline_Code/member.java
@@ -7,67 +7,67 @@ import java.util.Map;
 @SuppressWarnings("serial")
 public class member implements Serializable {
 
-	private String LN;
-	private String FN;
-	private String EM;
-	private int PN;
-	private int ID;
-	private double FINES;
+	private String LN;// changed LN to ln - darshan-08/20/2019
+	private String fn;// changed FN to fn - darshan-08/20/2019
+	private String em;// changed EM to em - darshan-08/20/2019
+	private int pn;// changed PN to pn-darshan-08/20/2019
+	private int id;// changed ID to id-darshan-08/20/2019
+	private double FINES;// changed FINE to fine-darshan-08/20/2019
 	
-	private Map<Integer, loan> LNS;
+	private Map<Integer, loan> lns;//changed LNS to lns -darshan-08/20/2019
 
 	
 	public member(String lastName, String firstName, String email, int phoneNo, int id) {
-		this.LN = lastName;
-		this.FN = firstName;
-		this.EM = email;
-		this.PN = phoneNo;
-		this.ID = id;
+		this.ln = lastName;
+		this.fn = firstName;
+		this.em = email;
+		this.pn = phoneNo;
+		this.id = id;
 		
-		this.LNS = new HashMap<>();
+		this.lns = new HashMap<>();
 	}
 
 	
-	public String toString() {
+	public String toString() { // all of variables between line 31 to 45 has been changed according to the changes had been done in the begining of the code.
 		StringBuilder sb = new StringBuilder();
-		sb.append("Member:  ").append(ID).append("\n")
-		  .append("  Name:  ").append(LN).append(", ").append(FN).append("\n")
-		  .append("  Email: ").append(EM).append("\n")
-		  .append("  Phone: ").append(PN)
+		sb.append("Member:  ").append(id).append("\n")
+		  .append("  Name:  ").append(ln).append(", ").append(fn).append("\n")
+		  .append("  Email: ").append(em).append("\n")
+		  .append("  Phone: ").append(pn)
 		  .append("\n")
-		  .append(String.format("  Fines Owed :  $%.2f", FINES))
+		  .append(String.format("  Fines Owed :  $%.2f", fines)) // changed FINES TO fines -darshan-08/20/2019
 		  .append("\n");
 		
-		for (loan LoAn : LNS.values()) {
-			sb.append(LoAn).append("\n");
+		for (loan loan : LNS.values()) { // changed LoAn to loan -darshan-08/20/2019
+			sb.append(loan).append("\n");
 		}		  
 		return sb.toString();
 	}
 
 	
-	public int GeT_ID() {
-		return ID;
+	public int GeT_ID() { // changed GET_ID to getid-darshan-08/20/2019
+		return id; // changed ID to id-darshan-08/20/2019
 	}
 
 	
-	public List<loan> GeT_LoAnS() {
-		return new ArrayList<loan>(LNS.values());
+	public List<loan> getLoans() {// changed GET_LoAns to getLoans-darshan-08/20/2019
+		return new ArrayList<loan>(lns.values());
 	}
 
 	
-	public int Number_Of_Current_Loans() {
-		return LNS.size();
+	public int number_Of_Current_Loans() { //changed Number_Of_Current_Loans to number_Of_loans-darshan-08/20/2019
+		return lns.size();
 	}
 
 	
-	public double Fines_OwEd() {
-		return FINES;
+	public double finesOwed() {// changed Fines_owes to finesOwed-darshan-08/20/2019
+		return fines;// changed FINE to fines-08/20/2019
 	}
 
 	
-	public void Take_Out_Loan(loan loan) {
-		if (!LNS.containsKey(loan.ID())) {
-			LNS.put(loan.ID(), loan);
+	public void take_Out_Loan(loan loan) { // changed Take_Out_Loan to take_Out_Loan-darshan-08/20/2019
+		if (!lns.containsKey(loan.ID())) {
+			lns.put(loan.ID(), loan);
 		}
 		else {
 			throw new RuntimeException("Duplicate loan added to member");
@@ -75,39 +75,39 @@ public class member implements Serializable {
 	}
 
 	
-	public String Get_LastName() {
-		return LN;
+	public String get_LastName() { // changed Get_lastName to get_LastName -darshan-08/20/2019
+		return ln;
 	}
 
 	
-	public String Get_FirstName() {
-		return FN;
+	public String get_FirstName() {// changed Get_FirstName to get_FristName-darshan-08/20/2019
+		return fn;
 	}
 
 
-	public void Add_Fine(double fine) {
-		FINES += fine;
+	public void add_Fine(double fine) { // changed Add_Fine to add_Fine-darshan-08/20/2019
+		fines += fine;
 	}
 	
-	public double Pay_Fine(double AmOuNt) {
-		if (AmOuNt < 0) {
+	public double pay_Fine(double AmOuNt) { // changed Pay_Fine to pay_Fine -darshan-08/20/2019
+		if (amount < 0) { // // changed AmOuNt to amount -darshan-08/20/2019
 			throw new RuntimeException("Member.payFine: amount must be positive");
 		}
 		double change = 0;
-		if (AmOuNt > FINES) {
-			change = AmOuNt - FINES;
-			FINES = 0;
+		if (amount > fines) {
+			change = amount - fines;
+			fines = 0;
 		}
 		else {
-			FINES -= AmOuNt;
+			fines -= AmOuNt;
 		}
 		return change;
 	}
 
 
-	public void dIsChArGeLoAn(loan LoAn) {
-		if (LNS.containsKey(LoAn.ID())) {
-			LNS.remove(LoAn.ID());
+	public void dIsChArGeLoAn(loan LoAn) { //changed LoAn to loan -darshan-08/20/2019
+		if (lns.containsKey(loan.ID())) {
+			lns.remove(loan.ID());
 		}
 		else {
 			throw new RuntimeException("No such loan held by member");
